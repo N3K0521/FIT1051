edd8dbe544bf245ed1ff604cdde0a8f547b9ffed
N3K0521 <wanghuixin0521@gmail.com>
2020-08-04 14:54:27 +0800

    enter

---
+++ Tutorial 1/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 10;
+        String message ;
+        message = "Welcome to FIT1051: ";
+        int sum;
+        sum = number1 + number2;
+        String result = message + sum;
+        System.out.println(result);
+        //System.out.println(message);
+        //System.out.println(sum);
+
+    }
+
+}
